5814a6aa539111e1e5d7238a5e078aacd9eaae49
mchemlal <michel.chemlal@gmail.com>
2021-01-23 13:44:25 +0100

    dernier commit à date

--- CompteEpargne.js
+++ CompteEpargne.js
@@ -1,23 +1,24 @@
 class CompteEpargne extends Compte {
 
-
+    /**
+     * constructeur du Compte Epargne qui hérite du constructeur de la classe Compte
+     * 
+     * @param {string} titulaire 
+     * @param {number} montant 
+     * @param {number} taux 
+     * @param {number} rythme 
+     */
     constructor(titulaire, montant = 50, taux = 0.005, rythme = 1) {
         super(titulaire, montant); // recuperation construteur classe mere
         this.taux = taux;
         this.rythme = rythme;
 
         /**
-         * interets de 0.005 % 
+         * interets de 0.005 % toute les 1 millisecondes
          */
         setInterval(() => {
             this.solde *= 1 + this.taux;
         }, this.rythme)
     }
-
-
-
-
-
-
 
 }
